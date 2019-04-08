1e9e4594215bff591bf743df57ca9a8c98060f54
tmhernan <tiffany.hernandez@asu.edu>
2019-04-08 14:55:28 -0700

    Fixed files with Checkstyle. Violations below 25 in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
+import main.java.Major;
+import main.java.Student;
+
 public class Main {
     static HashMap<String, Student> students = new HashMap<>();
     static HashMap<String, CourseGrades0> courses = new HashMap<>();
@@ -19,9 +22,9 @@ public class Main {
         students.put("rebecca", new Student("rebecca", Major.valueOf("SER")));
         students.put("alex", new Student("alex", Major.valueOf("SER")));
         students.put("rose", new Student("rose", Major.valueOf("SER")));
-        
-        
-        
+
+
+
         courses.put("SER316", new CourseGrades0("SER316"));
         courses.put("SER315", new CourseGrades0("SER315"));
         courses.put("SC232", new CourseGrades0("CS232"));
@@ -34,10 +37,10 @@ public class Main {
         courses.get("SER315").set_points("rebecca", 10);
         courses.get("SER315").set_points("alex", 30);
         courses.get("SER315").set_points("rose", 23);
-        
-        
+
+
         courses.get("SER315").printCourseStats();
 
-     
+
     }
 }
